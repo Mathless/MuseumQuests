b48b31ac0563380dbe19d1644f918366c03da992
Stepan <kryzhankov.stepan@mail.ru>
2019-06-05 17:28:29 +0300

    Recommit

---
+++ app/src/main/java/com/example/MuseumQuests/QuestInfo.kt
@@ -0,0 +1,12 @@
+package com.example.MuseumQuests
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class QuestInfo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quest_info)
+    }
+}
---
+++ app/src/main/java/com/example/MuseumQuests/QuestList.kt
@@ -0,0 +1,12 @@
+package com.example.MuseumQuests
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class QuestList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quest_list)
+    }
+}
