8bc0cd19fa87ad29f3c758c8d12dc94b6c1c7643
Stepan <kryzhankov.stepan@mail.ru>
2019-06-06 11:20:57 +0300

    activity_start added

---
+++ app/src/main/java/com/example/MuseumQuests/Result.kt
@@ -0,0 +1,24 @@
+package com.example.MuseumQuests
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_result.*
+
+class Result : AppCompatActivity() {
+
+    val logTag = "DEMO_TAG"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        Log.d(logTag, "onCreate called")
+        gotohomefromresult.setOnClickListener {
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
